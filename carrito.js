ae51e89f85300095bf100e4061739b5379558cbd
Alejo Garcia <alejogarciacar@gmail.com>
2022-09-04 16:23:40 -0300

    form

--- carrito.js
+++ carrito.js
@@ -3,21 +3,6 @@ const detalle = document.querySelector("#detalle")
 const divCarrito = document.querySelector("#carrito")
 const vaciar = document.getElementById("vaciar")
 
-/* const grupo = groupBy(carrito2, producto => producto.id) || [];
-console.log(grupo)
-function groupBy(list, keyGetter) {
-    const map = new Map();
-    list.forEach((item) => {
-        const key = keyGetter(item);
-        const collection = map.get(key);
-        if (!collection) {
-            map.set(key, [item]);
-        } else {
-            collection.push(item);
-        }
-    });
-    return map;
-} */
 function productosCarrito() {
 
     carrito2.map(producto => {
--- index.js
+++ index.js
@@ -1,123 +1,71 @@
 
+//validacion de formulario de contacto
+console.log(document.forms)
 
-/* for(let i =10; i >=0; i--){
-    if(i===10){
-        console.log( `La Bomba Explotara en:  ${i}`)
-    }
-    else if (i<=9 && i !== 0){
-        console.log( `                     :  ${i}`)
-    }
-    else if (i===0) {
-        console.log(`                     BOOM!`)
+const formulario = document.querySelector("#formulario")
+const inputNombre = document.querySelector("#nombre")
+const inputEmail = document.querySelector("#email")
+const inputMensaje = document.querySelector("#mensaje")
+const submit = document.querySelector("#submit")
+
+console.log(formulario, inputNombre, inputEmail, inputMensaje)
+
+const clientes = []
+
+class Cliente {
+    constructor(nombre, email, mensaje) {
+        this.nombre = nombre;
+
+
+        this.email = email;
+        this.mensaje = mensaje;
     }
 }
 
-/* /* /* /* const respuesta = prompt("Escriba mayor, para saber a partir de que edad es mayor, o menor, en caso contrario")
+formulario.onsubmit = (event) => {
+    event.preventDefault()
+    console.log(event)
+    clientes.push(new Cliente(inputNombre.value, inputEmail.value, inputMensaje.value))
+    console.log(clientes)
 
-    for(let i=1; i<=100; i++){
-        
-        if(respuesta === "mayor" && i>=18){
-            console.log(`Si usted tiene ${i} años, es mayor`)
-        }
-        else if(respuesta ==="menor"  && i<18){
-            console.log(`Si usted tiene ${i} años, es menor`)
-        }
-        else if(respuesta ===null || respuesta !=="menor" ||respuesta !=="mayor"){
-            console.log("No Ingreso Nada Coherente")
-        }
+    formulario.reset()
+}
+
+inputNombre.onchange = () => {
+
+    if (inputNombre.value.length <= 2) {
+        alert("Su Nombre es muy corto")
+        inputNombre.style.border = "2px solid red";
+        submit.disabled = true;
+
+    } else {
+
+        inputNombre.style.border = "2px solid green";
+        submit.disabled = false;
     }
- */ 
-/* const ingresePrecio =parseInt( prompt("Ingrese el Precio del Producto Seleccionado"));
-    const suma = (precioProducto, iva) =>precioProducto+ iva;
-    const resta = (precioProducto, descuento) => precioProducto-descuento;
-        let precioProducto= ingresePrecio;
-        let descuento= 125
-        const iva= precioIngresado => precioProducto * 0.21;
-        let nuevoPrecio = resta (suma (precioProducto, iva(precioProducto)), descuento);
-        alert(nuevoPrecio); 
- */
-    console.log(document.forms)
+}
 
-        const formulario = document.querySelector("#formulario")
-        const inputNombre = document.querySelector("#nombre")
-        const inputApellido = document.querySelector("#apellido")
-        const inputAsunto = document.querySelector("#asunto")
-        const inputEmail = document.querySelector("#email")
-        const inputMensaje = document.querySelector("#mensaje")
-        const submit = document.querySelector("#submit")
-        
-        console.log(formulario, inputNombre, inputApellido, inputAsunto, inputEmail, inputMensaje)
-        
-    const clientes = []
-        
-        class Cliente {
-            constructor(nombre, apellido, asunto, email, mensaje ){
-                this.nombre = nombre;
-                this.apellido = apellido;
-                this.asunto = asunto;
-                this.email = email;
-                this.mensaje = mensaje; 
-            }
-        }
 
-        formulario.onsubmit = (event) => {
-            event.preventDefault()
-            console.log(event)
-            clientes.push(new Cliente(inputNombre.value, inputApellido.value, inputAsunto.value, inputEmail.value, inputMensaje.value))
-            console.log(clientes)
 
-            formulario.reset()
-        }
+inputEmail.onchange = () => {
 
-        inputNombre.onchange = () => {  
-            console.log(inputNombre.value)
-            if (inputNombre.value.length <= 2){
-                alert("Su Nombre es muy corto")
-                inputNombre.style.border = "4px solid red"
-                submit.disabled = true
-                
-            } else {
-                
-                inputNombre.style.border = "4px solid green"
-                submit.disabled = false
-            }
-        }
+    if (inputEmail.value.length <= 2) {
 
-        inputApellido.onchange = () => {  
-            console.log(inputApellido.value)
-            if (inputApellido.value.length <= 2){
-                alert("Su apellido es muy corto")
-                inputApellido.style.border = "4px solid red"
-                submit.disabled = true
-            } else {
-                
-                inputApellido.style.border = "4px solid green"
-                submit.disabled = false
-            }
-        }
+        inputEmail.style.border = "2px solid red";
+        submit.disabled = true;
+    } else {
+        inputEmail.style.border = "2px solid green";
+        submit.disabled = false;
+    }
+}
+inputMensaje.onchange = () => {
 
-        inputAsunto.onchange = () => {  
-            console.log(inputAsunto.value)
-            if (inputAsunto.value.length <= 2){
-                
-                inputAsunto.style.border = "4px solid red"
-                submit.disabled = true
-            } else {
-                
-                inputAsunto.style.border = "4px solid green"
-                submit.disabled = false
-            }
-        }
+    if (inputMensaje.value.length <= 2) {
 
-        
-        inputEmail.onchange = () => {  
-            console.log(inputEmail.value)
-            if (inputEmail.value.length <= 2){
-                
-                inputEmail.style.border = "4px solid red"
-                submit.disabled = true
-            } else {
-                inputEmail.style.border = "4px solid green"
-                submit.disabled = false
-            }
-        }
+        inputMensaje.style.border = "2px solid red";
+        submit.disabled = true;
+    } else {
+        inputMensaje.style.border = "2px solid green";
+        submit.disabled = false;
+    }
+}
